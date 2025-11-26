1760ea4f636d9731dab36d82d486fae16c14a72b
Mradul Krishna Bhardwaj <chhayabhardwaj111@gmail.com>
2025-11-26 22:06:43 +0530

    Time: 49 ms (74.17%), Space: 148.1 MB (19.17%) - LeetHub

---
+++ 3295-report-spam-message/3295-report-spam-message.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean reportSpam(String[] message, String[] bannedWords) {
+        HashSet<String> set = new HashSet<>();
+
+        for (String s : bannedWords) {
+            set.add(s);
+        }
+
+        int count = 0;
+        for (String word : message) {
+            if (set.contains(word)) {
+                count++;
+                if (count >= 2) return true;
+            }
+        }
+
+        return false;
+    }
+}
