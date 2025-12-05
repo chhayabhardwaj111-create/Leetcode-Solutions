063f438621d5ebb4d729dc8a6d7c62b3a561335b
Mradul Krishna Bhardwaj <chhayabhardwaj111@gmail.com>
2025-12-05 15:02:41 +0530

    Time: 0 ms (100%), Space: 45.6 MB (25.91%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] t = new char[indices.length];
+
+        for(int i = 0; i < indices.length; i++){
+            t[indices[i]] = s.charAt(i);
+        }
+
+        String sc = new String(t);
+
+        return sc;
+
+    }
+}
