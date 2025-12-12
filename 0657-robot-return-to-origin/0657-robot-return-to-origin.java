4070fdd4e3b6fe840c220f135ce899c01636efdb
Mradul Krishna Bhardwaj <chhayabhardwaj111@gmail.com>
2025-12-12 19:45:30 +0530

    Time: 8 ms (23.47%), Space: 45.5 MB (52.6%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,7 +1,5 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        boolean vertical = false;
-        boolean horizontal = false;
         int up = 0;
         int down = 0; 
         int right = 0; 
@@ -17,18 +15,9 @@ class Solution {
                 left++;
             }
         }
-        if(up == down){
-            vertical = true;
-        }
-        if(right == left){
-            horizontal = true;
-        }
-
-        if(horizontal == true && vertical == true){
+        if(up == down && right == left){
             return true;
         }
-
         return false;
-
     }
 }
