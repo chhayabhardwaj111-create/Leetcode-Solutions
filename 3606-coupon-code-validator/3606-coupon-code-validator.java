2759785f9ada8cf7601fb90e5cd332d5de65bbdc
Mradul Krishna Bhardwaj <chhayabhardwaj111@gmail.com>
2025-12-13 20:30:32 +0530

    Time: 7 ms (89.09%), Space: 47.5 MB (42.27%) - LeetHub

--- 3606-coupon-code-validator/3606-coupon-code-validator.java
+++ 3606-coupon-code-validator/3606-coupon-code-validator.java
@@ -2,32 +2,32 @@ import java.util.*;
 
 class Solution {
     public List<String> validateCoupons(String[] code, String[] businessLine, boolean[] isActive) {
-        Set<String> allowed = new HashSet<>();
-        allowed.add("electronics");
-        allowed.add("grocery");
-        allowed.add("pharmacy");
-        allowed.add("restaurant");
 
-        List<String[]> list = new ArrayList<>();
+        HashSet<String> li1 = new HashSet<>();
+        li1.add("electronics");
+        li1.add("grocery");
+        li1.add("pharmacy");
+        li1.add("restaurant");
+
+        ArrayList<String[]> li2 = new ArrayList<>();
 
         for (int i = 0; i < code.length; i++) {
-            if (isActive[i] && allowed.contains(businessLine[i]) && valid(code[i])) {
-                list.add(new String[]{businessLine[i], code[i]});
+            if (isActive[i] && li1.contains(businessLine[i]) && str(code[i])) {
+                li2.add(new String[]{businessLine[i], code[i]});
             }
         }
 
-        Collections.sort(list, (a, b) -> {
+        Collections.sort(li2, (a, b) -> {
             if (!a[0].equals(b[0])) return a[0].compareTo(b[0]);
             return a[1].compareTo(b[1]);
         });
 
-        List<String> ans = new ArrayList<>();
-        for (String[] x : list) ans.add(x[1]);
-
-        return ase;
+        ArrayList<String> ans = new ArrayList<>();
+        for (String[] x : li2) ans.add(x[1]);
+        return ans;
     }
 
-    private boolean valid(String s) {
+    private boolean str(String s) {
         if (s.length() == 0) return false;
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
