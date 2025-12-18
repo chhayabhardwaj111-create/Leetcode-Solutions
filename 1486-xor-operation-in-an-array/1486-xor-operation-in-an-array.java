d8a8c4ded66f319a6b2ef5394c67527c14109128
Mradul Krishna Bhardwaj <chhayabhardwaj111@gmail.com>
2025-12-18 16:22:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public int xorOperation(int n, int start) {
         int[] arr = new int[n];
-        int ans = 0;
+        int ans = ;
 
         for(int i = 0; i < n; i++){
             arr[i] = start + 2 * i;
