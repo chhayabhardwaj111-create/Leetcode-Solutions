77db256a463828d53d67e2abc4dfe6c3a88dc9ff
Mradul Krishna Bhardwaj <chhayabhardwaj111@gmail.com>
2025-12-18 12:01:39 +0530

    Time: 0 ms (100%), Space: 42.1 MB (57.3%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findComplement(int num) {
+        int count = 0;
+        int temp = num;
+
+        while(temp > 0){
+            temp = temp >> 1;
+            count++;
+        }
+
+        int mask = (1 << count) - 1;
+
+        return num ^ mask;
+        
+
+    }
+}
