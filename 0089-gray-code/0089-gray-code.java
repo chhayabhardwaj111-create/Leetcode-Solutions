74664180da4b34b761844030442b9cf7739e8736
Mradul Krishna Bhardwaj <chhayabhardwaj111@gmail.com>
2025-12-20 11:26:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.java
@@ -0,0 +1,10 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> li = new ArrayList<>();
+                
+        for(int i = 0; i < (1 << n); i++){
+            li.add(i ^ (i >> 1));
+        }
+        return l;
+    }
+}
